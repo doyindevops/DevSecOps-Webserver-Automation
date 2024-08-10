e760c3f2fa66cc52e786faba1844860fff67b5cf
Adedoyin Ekong <adedoyindevops@gmail.com>
2024-08-10 03:30:22 +0100

    Update websetup.sh

--- websetup.sh
+++ websetup.sh
@@ -4,34 +4,28 @@
 echo "########################################"
 echo "Installing packages."
 echo "########################################"
-sudo apt update -y > /dev/null
-sudo apt install wget unzip apache2 -y > /dev/null
-echo wqa
-;]'\
-5+
+sudo yum update -y > /dev/null
+sudo yum install wget unzip httpd -y > /dev/null
+
 # Creating Temp Directory 
 echo "########################################"
 echo "Starting Artifact Deployment"
 echo "########################################"
 mkdir -p /tmp/webfiles
 cd /tmp/webfiles
-echo
 
 wget https://www.tooplate.com/zip-templates/2133_moso_interior.zip > /dev/null
 unzip 2133_moso_interior.zip  > /dev/null
 sudo cp -r 2133_moso_interior/* /var/www/html/
-echo
 
 # Bounce Service
 echo "########################################"
 echo "Restarting Apache Webserver service"
 echo "########################################"
-sudo systemctl restart apache2
-echo
+sudo systemctl restart httpd
 
 # Clean Up
 echo "########################################"
 echo "Removing Temporary Files"
 echo "########################################"
 sudo rm -rf /tmp/webfiles
-echo
